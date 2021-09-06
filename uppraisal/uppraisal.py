34c581d1c07dc67fd3258d70dd5146305ca1ffba
Erik Kemperman <kemperman@rsm.nl>
2021-09-06 14:53:34 +0200

    Some refactoring

--- uppraisal/uppraisal.py
+++ uppraisal/uppraisal.py
@@ -1,18 +1,29 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import json
 import time
+import typing as tp
 
 import requests
 from tqdm import tqdm
 
-from uppraisal.helpers import chunker
-from uppraisal.helpers import getexcel
+from .constants import *
+from .helpers import chunker
+from .helpers import get_excel
 
 
-def upload_appraisal(cv_access_token, workbook_path, cv_id_course, cv_id_assignment,
-                     submission_comment_header="submission_comment", grade_header="grade", workbook_tab='Sheet1'):
-    """Upload assignment comments and grades from an excel workbook, one assignment at a time.
+# TODO This is not used?
+def upload_appraisal(
+    cv_access_token: str,
+    workbook_path: str,
+    cv_course_id: int,
+    cv_assignment_id: int,
+    submission_comment_header: str = DEFAULT_SUBMISSION_COMMENT_HEADER,
+    grade_header: str = DEFAULT_GRADE_HEADER,
+    workbook_tab: str = DEFAULT_WORKBOOK_TAB
+) -> tp.Dict[int, tp.Any]:
+    """
+    Upload assignment comments and grades from an excel workbook, one assignment at a time.
 
     :param cv_access_token: Canvas access token (generally 70 characters in length; see
     https://canvas.instructure.com/courses/785215/pages/getting-started-with-the-api).
@@ -20,10 +31,10 @@ def upload_appraisal(cv_access_token, workbook_path, cv_id_course, cv_id_assignm
     :param workbook_path: File path to workbook containing grade and comment data, with Canvas user_id's included
     under the header "user_id".
     :type workbook_path: str
-    :param cv_id_course: Course identifier (see Canvas course URL).
-    :type cv_id_course: int
-    :param cv_id_assignment: Assignment identifier (see Canvas assignment URL).
-    :type cv_id_assignment: int
+    :param cv_course_id: Course identifier (see Canvas course URL).
+    :type cv_course_id: int
+    :param cv_assignment_id: Assignment identifier (see Canvas assignment URL).
+    :type cv_assignment_id: int
     :param submission_comment_header: Header of the submission comment column in the Excel workbook. Default is
     "submission_comment".
     :type submission_comment_header: str
@@ -34,43 +45,62 @@ def upload_appraisal(cv_access_token, workbook_path, cv_id_course, cv_id_assignm
     :return: Dictionary with updated assignment meta data or an error message
     :rtype: dict
     """
+
     fields = ["user_id", grade_header, submission_comment_header]
-    data = getexcel(workbook_path, workbook_tab, include_fieldnames=fields, return_headers=False)
+    data = get_excel(workbook_path, workbook_tab, include_fieldnames=fields, return_headers=False)
     for i, row in enumerate(data):
         data[i][0] = int(row[0])
-    r_data = {}
-    for cv_id_user, grade, submission_comment in data:
+    responses = {}
+    for cv_user_id, grade, submission_comment in data:
         params = {
-            "comment": {"text_comment": submission_comment},
-            "submission": {"posted_grade": grade}
+            'comment': {'text_comment': submission_comment},
+            'submission': {'posted_grade': grade}
         }
-        r = requests.put('https://canvas.eur.nl/api/v1/courses/{cid}/assignments/{aid}/submissions/{uid}'.format(
-            cid=cv_id_course,
-            aid=cv_id_assignment,
-            uid=cv_id_user
-        ),
-            headers={'Authorization': 'Bearer ' + cv_access_token, 'Content-type': 'application/json'},
+
+        url = CANVAS_SUBMISSION_URL.format(
+            cid=cv_course_id,
+            aid=cv_assignment_id,
+            uid=cv_user_id
+        )
+        response = requests.put(
+            url,
+            headers={
+                'Authorization': 'Bearer ' + cv_access_token,
+                'Content-type': 'application/json'
+            },
             json=params,
         )
-        time.sleep(.02)  # TODO: Check whether the delay is sufficient
-        r_data[cv_id_user] = json.loads(r.content.decode('utf-8'))
-    return r_data
+        if response.status_code // 100 != 2:
+            raise Exception(f'Got an error {response.status_code} from {url}: {response.text}')
+
+        time.sleep(SLEEP_UPLOAD_APPRAISAL)
+        responses[cv_user_id] = json.loads(response.text)
+
+    return responses
 
 
-def upload_appraisals(cv_access_token, workbook_path, cv_id_course, cv_id_assignment,
-                      submission_comment_header="submission_comment", grade_header="grade", workbook_tab='Sheet1'):
-    """Upload assignment comments and grades from an excel workbook, with multiple assignments per post.
+def upload_appraisals(
+    in_filepath: str,
+    cv_access_token: str,
+    cv_course_id: int,
+    cv_assignment_id: int,
+    submission_comment_header: str = DEFAULT_SUBMISSION_COMMENT_HEADER,
+    grade_header: str = DEFAULT_GRADE_HEADER,
+    workbook_tab: str = DEFAULT_WORKBOOK_TAB
+) -> tp.List[tp.Any]:
+    """
+    Upload assignment comments and grades from an excel workbook, with multiple assignments per post.
 
+    :param in_filepath: File path to workbook containing grade and comment data, with Canvas user_id's
+    included under the header "user_id".
+    :type in_filepath: str
     :param cv_access_token: Canvas access token (generally 70 characters in length; see
     https://canvas.instructure.com/courses/785215/pages/getting-started-with-the-api).
     :type cv_access_token: str
-    :param workbook_path: File path to workbook containing grade and comment data, with Canvas user_id's
-    included under the header "user_id".
-    :type workbook_path: str
-    :param cv_id_course: Course identifier (see Canvas course URL).
-    :type cv_id_course: int
-    :param cv_id_assignment: Assignment identifier (see Canvas assignment URL).
-    :type cv_id_assignment: int
+    :param cv_course_id: Course identifier (see Canvas course URL).
+    :type cv_course_id: int
+    :param cv_assignment_id: Assignment identifier (see Canvas assignment URL).
+    :type cv_assignment_id: int
     :param submission_comment_header: Header of the submission comment column in the Excel workbook. Default is
     "submission_comment".
     :type submission_comment_header: str
@@ -81,36 +111,61 @@ def upload_appraisals(cv_access_token, workbook_path, cv_id_course, cv_id_assign
     :return: Dictionary with updated assignment meta data or an error message
     :rtype: dict
     """
-    fields = ["user_id", grade_header, submission_comment_header]
-    data = getexcel(workbook_path, workbook_tab, include_fieldnames=fields, return_headers=False)
+
+    fields = ['user_id', grade_header, submission_comment_header]
+    data = get_excel(
+        in_filepath,
+        workbook_tab,
+        include_fieldnames=fields,
+        return_headers=False
+    )
+
     for i, row in enumerate(data):
         data[i][0] = int(row[0])
-    r_data = []
-    for data_chunk in tqdm(chunker(data, 100)):
-        params = {"grade_data": {}}
+
+    responses = []
+    for data_chunk in tqdm(chunker(data)):
+        params = {'grade_data': {}}
         for cv_id_user, grade, submission_comment in data_chunk:
-            params["grade_data"][int(cv_id_user)] = {
+            params['grade_data'][int(cv_id_user)] = {
                 # TODO: Create a test to verify robustness against floating point imprecision errors
-                "posted_grade": str(grade),
+                'posted_grade': str(grade),
                 # TODO: Check encoding and non-ASCII character issues
-                "text_comment": submission_comment
+                'text_comment': submission_comment
             }
-        r = requests.post(
-            'https://canvas.eur.nl/api/v1/courses/{cid}/assignments/{aid}/submissions/update_grades'.format(
-                cid=cv_id_course,
-                aid=cv_id_assignment
-            ),
-            headers={'Authorization': 'Bearer ' + cv_access_token, 'Content-type': 'application/json'},
-            json=params,
+
+        url = CANVAS_UPDATE_GRADES_URL.format(cid=cv_course_id, aid=cv_assignment_id)
+        response = requests.post(
+            url,
+            headers={
+                'Authorization': 'Bearer ' + cv_access_token,
+                'Content-type': 'application/json'
+            },
+            json=params
         )
-        r_dat = json.loads(r.content.decode('utf-8'))
+        if response.status_code // 100 != 2:
+            raise Exception(f'Got an error {response.status_code} from {url}: {response.text}')
+
+        body = json.loads(response.text)
         state = 'queued'
         while state == 'queued':
-            time.sleep(30)
-            progress = requests.get(r_dat['url'],
-                                    headers={'Authorization': 'Bearer ' + cv_access_token,
-                                             'Content-type': 'application/json'})
-            progress = json.loads(progress.content.decode('utf-8'))
+            time.sleep(SLEEP_UPLOAD_APPRAISALS)
+            url = body['url']
+            response = requests.get(
+                url,
+                headers={
+                    'Authorization': 'Bearer ' + cv_access_token,
+                    'Content-type': 'application/json'
+                }
+            )
+            if response.status_code // 100 != 2:
+                raise Exception(
+                    f'Got an error {response.status_code} from {url}: {response.text}')
+
+            progress = json.loads(response.text)
             state = progress['workflow_state']
-        r_data.append(r_dat)
-    return r_data
+
+        responses.append(body)
+
+    return responses
+
